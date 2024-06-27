ac51bc1e4f80fc8d2b3cf7e8d429b8125d27abc6
Michael Yagi <michaeltyagi@gmail.com>
2024-06-27 14:41:46 -0700

    Adjustment in webglearth js

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
 return array(
-    'shashinUrl' => '<shashin_url>',
-    'shashinApiKey' => '<shashin_api_key>'
+    'shashinUrl' => 'http://192.168.0.199:6624',
+    'shashinApiKey' => 'e456ff02-81ad-39e0-b3bb-a8b4802d8392'
 );

5b1d0c0f991bc740dbf54aa971d5eafff48fc760
Michael Yagi <michaeltyagi@gmail.com>
2025-11-03 15:11:41 -0800

    Updating config

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
 return array(
-    'shashinUrl' => '<shashin_url>',
-    'shashinApiKey' => '<shashin_api_key>'
+    'shashinUrl' => 'http://192.168.0.199:6624',
+    'shashinApiKey' => '637d23ae-63b1-3591-b968-2191af2e279a' // user - 0908717c-ce53-3ef2-a8f2-fdb4c55bdf1f super - 637d23ae-63b1-3591-b968-2191af2e279a
 );

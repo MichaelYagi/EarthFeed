1e010780b4576a01c0a73ab7b67a72310f06bbdb
Michael Yagi <michaeltyagi@gmail.com>
2025-11-04 18:59:25 -0800

    Added logic so popups appear 1 at a time

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 
+// Download and setup server at https://api.repoflow.io/browse/universal/cd373a98-3f60-419f-9c40-d10a1180ccda/shashin/shashin
 return array(
     'shashinUrl' => 'http://192.168.0.199:6624',
     'shashinApiKey' => '637d23ae-63b1-3591-b968-2191af2e279a' // user - 0908717c-ce53-3ef2-a8f2-fdb4c55bdf1f super - 637d23ae-63b1-3591-b968-2191af2e279a
